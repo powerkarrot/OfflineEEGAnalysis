9a3ae782a988552b67b26b1ed9fb5068c6b7d908
powerkarrot <powerkarrot@gmail.com>
2022-11-11 21:17:10 +0100

    working pipeline 1

--- Pipeline_1/EEG_pipeline_1.py
+++ Pipeline_1/EEG_pipeline_1.py
@@ -8,6 +8,7 @@ import tqdm
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import seaborn as sns
+import autoreject
 from autoreject import get_rejection_threshold
 from pathlib import Path
 from itertools import chain, repeat
@@ -113,81 +114,100 @@ if len(os.listdir('./fifs')) != NUM_BLOCKS * len(lstPIds):
         #    continue
         # if (pid > 2):
         #         break
-        
-        dfState = pd.read_csv(f"{path}ID{pid}-state.csv")
-        dfState = pd.read_csv(f"{path}ID{pid}-state.csv")
-            
-        dfEEG = pd.read_csv(f"{path}ID{pid}-EEG.csv")
-        dfEEG = dfEEG.rename(columns={"Value0": "F3", "Value1": "C3", "Value2": "P3", "Value3": "P4", "Value4": "C4", "Value5": "F4", "Value6": "Pz"})
-        dfEEG.drop("TimeLsl", axis =1, inplace=True)
+        if len(os.listdir('./fifs/')) != NUM_BLOCKS * len(lstPIds):
 
-        dstate = pd.read_csv(f"{path}ID{pid}-state.csv")
         
-        dfAll = pd.merge(dfEEG, dstate, on =["Time"], how="outer")
-        dfAll = dfAll.sort_values(by="Time") # inplace?
-        
-        dfAll = dfAll.drop(columns=["Value7","AdaptationStatus", "NBackN", "State"] )
-        dfAll.fillna(method='ffill', inplace=True)
-        dfAll = dfAll.drop(dfAll[dfAll.BlockNumber < 0].index)
-        dfAll = dfAll.dropna()
+            dfState = pd.read_csv(f"{path}ID{pid}-state.csv")
+            dfState = pd.read_csv(f"{path}ID{pid}-state.csv")
+                
+            dfEEG = pd.read_csv(f"{path}ID{pid}-EEG.csv")
+            dfEEG = dfEEG.rename(columns={"Value0": "F3", "Value1": "C3", "Value2": "P3", "Value3": "P4", "Value4": "C4", "Value5": "F4", "Value6": "Pz"})
+            dfEEG.drop("TimeLsl", axis =1, inplace=True)
 
-        for x in range(1, NUM_BLOCKS+1):  
+            dstate = pd.read_csv(f"{path}ID{pid}-state.csv")
             
-            # if(x > 1):
-            #     break
+            dfAll = pd.merge(dfEEG, dstate, on =["Time"], how="outer")
+            dfAll = dfAll.sort_values(by="Time") # inplace?
             
-            # Prepare data 
-            # see if fif already present, else filter / clean data and save it
+            dfAll = dfAll.drop(columns=["Value7","AdaptationStatus", "NBackN", "State"] )
+            dfAll.fillna(method='ffill', inplace=True)
+            dfAll = dfAll.drop(dfAll[dfAll.BlockNumber < 0].index)
+            dfAll = dfAll.dropna()
+
+            for x in range(1, NUM_BLOCKS+1):  
+                
+                # if(x > 1):
+                #     break
+                
+                # Prepare data 
+                # see if fif already present, else filter / clean data and save it
+                        
+                data = dfAll.loc[dfAll['BlockNumber'] == x]
+                df = pd.DataFrame(data)
+                # data.plot(x="Time", y=["F3", "C3","P3","P3","C4","F4","Pz"])
+
+                sfreq=300
+                info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
+                info.set_montage('standard_1020',  match_case=False)
+
+                samples = df.T
+                
+                raw = mne.io.RawArray(samples, info)
+                raw.drop_channels(['Time', 'BlockNumber'])
+                
+                # remove power line interferance
+                raw.notch_filter(50., n_jobs=-1)
+                
+                # high pass filter to remove slow drifts, 70 Hz low pass
+                raw.filter(1., 70, None, fir_design='firwin')
+                
+                # set EEG reference
+                raw.set_eeg_reference('average', projection=True)
+                #raw.set_eeg_reference(ref_channels=['Pz'])
+                
+
+                # Visual inspection of bad channels
+                # TODO, empty for now. With new setup, check for bad channels only once for all blocks.
+                raw.info['bads'] =  bads[pid-1][x-1]
+                if raw.info['bads']:
+                    raw.interpolate_bads()
+                
+                #raw.plot(scalings='20e+4')
+                # raw.plot( scalings='20e-4', n_channels = 7, lowpass=bands.alpha[0], highpass=bands.alpha[1])
+                # raw.plot_psd()
+                
+                # Create equal length epochs of 4 seconds
+                epochs = mne.make_fixed_length_epochs(raw.copy(), preload=True, duration = 4)
+                
+                #evoked.plot_joint(picks='eeg')
+                #evoked.plot_topomap(times=[0., 10., 20., 30., 90.], ch_type='eeg')
+                
+                # Autoreject based on rejection threshold
+                
+                reject = get_rejection_threshold(epochs, ch_types = 'eeg')
+                #print("The rejection dictionary is %s " %reject)
+                #
+                epochs.drop_bad(reject=reject)
+                #epochs.plot_drop_log()
+                #epochs.average().plot()
+                if(False):
+                    ar = autoreject.AutoReject(n_jobs=-1,  verbose=True, random_state=11)
                     
-            data = dfAll.loc[dfAll['BlockNumber'] == x]
-            df = pd.DataFrame(data)
-            # data.plot(x="Time", y=["F3", "C3","P3","P3","C4","F4","Pz"])
-
-            sfreq=300
-            info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
-            info.set_montage('standard_1020',  match_case=False)
-
-            samples = df.T
-            
-            raw = mne.io.RawArray(samples, info)
-            raw.drop_channels(['Time', 'BlockNumber'])
-            
-            # high pass filter to remove slow drifts, 70 Hz low pass
-            raw.filter(1., 70, None, fir_design='firwin')
-            
-            # remove power line interferance
-            raw.notch_filter(50, n_jobs=-1)
-            
-            # set EEG reference
-            raw.set_eeg_reference('average', projection=True)
-            #raw.set_eeg_reference(ref_channels=['Pz'])
-            
-            # Visual inspection of bad channels
-            # TODO, empty for now. With new setup, check for bad channels only once for all blocks.
-            raw.info['bads'] =  bads[pid-1][x-1]
-            if raw.info['bads']:
-                raw.interpolate_bads()
-            
-            #raw.plot(scalings='20e+4')
-            # raw.plot( scalings='20e-4', n_channels = 7, lowpass=bands.alpha[0], highpass=bands.alpha[1])
-            # raw.plot_psd()
-            
-            # Create equal length epochs of 4 seconds
-            epochs = mne.make_fixed_length_epochs(raw.copy(), preload=False, duration = 4)
-            #evoked.plot_joint(picks='eeg')
-            #evoked.plot_topomap(times=[0., 10., 20., 30., 90.], ch_type='eeg')
-            
-            # Autoreject based on rejection threshold
-            #reject = dict(eeg=400e-6)  # unit: uV (EEG channels) dont forget the sample conversion to uV
-            reject = get_rejection_threshold(epochs, ch_types = 'eeg')
-            
-            reject['eeg'] = reject['eeg']
-            print("The rejection dictionary is %s " %reject)
-            epochs.drop_bad(reject=reject)  
-            #epochs.plot_drop_log()
-            
-            #arr_epochs.append(epochs)
-            epochs.save('./fifs/' + str(pid) + '-' + str(x) + '-epo.fif', overwrite = True)
+                    #ar.fit(epochs_ica)
+                
+    #                #reject_log.plot('horizontal')
+                
+                    #evoked_bad = epochs[reject_log.bad_epochs].average()
+                    #evoked_bad = epochs_clean.average()
+    
+                    #plt.figure()
+                    #plt.plot(evoked_bad.times, evoked_bad.data.T * 1e6, 'r', zorder=-1)
+                    #epochs_ar.average().plot(axes=plt.gca())
+                    
+                    epochs_clean = ar.fit_transform(epochs_ica) 
+                
+                #arr_epochs.append(epochs)
+                epochs.save('./fifs/' + str(pid) + '-' + str(x) + '-epo.fif', overwrite = True)
 
 
 
@@ -210,14 +230,13 @@ for pid in tqdm.tqdm(lstPIds):
                 
     for x in range(1, NUM_BLOCKS+1):  
         
-        epochs = mne.read_epochs('./fifs/' + str(pid) + '-' + str(x) + '-epo.fif')
+        epochs = mne.read_epochs('./fifs/' + str(pid) + '-' + str(x) + '-epo.fif', preload=True)
 
         # should probably delete contents first but hey
         if len(os.listdir('./ica/fifs/')) != NUM_BLOCKS * len(lstPIds):
             # independent component analysis (ICA)
-            ica = mne.preprocessing.ICA(method="fastica", n_components=5, random_state=97, max_iter='auto')
+            ica = mne.preprocessing.ICA(method="fastica", n_components = 5, random_state=97, max_iter='auto')
 
-            epochs.load_data()
             ica.fit(epochs)
             #ica.fit(epochs, reject=reject) # to reject or not reject, that is the question
             
@@ -231,8 +250,8 @@ for pid in tqdm.tqdm(lstPIds):
             done = False
             while not done:
                 
-                ica.plot_properties(epochs, dB= True, log_scale= False, psd_args={'fmax':30})
-                ica.plot_sources(epochs, block = True, title = str(pid) + '-' + str(x), stop = 118 )
+                ica.plot_properties(epochs, dB= True, log_scale= True, psd_args={'fmax':70})
+                ica.plot_sources(epochs, block = True, title = str(pid) + '-' + str(x), stop = 360. )
                 ics_old = ica.exclude
 
                 ica.plot_overlay(epochs.average(), exclude=exclude_ic, picks='eeg', stop = 360.)
@@ -326,9 +345,12 @@ for i, n in enumerate(icas):
         n.plot_overlay(arr_epochs[i].average(), n.labels_['exclude'], picks='eeg', title=("Pid "+ str(p) +" block " +str(b)) )
         n.exclude = n.labels_['exclude'] # do i need to do this again..? - i think so? plot overlay still plots it
         # apparently this returns None??
-        n.apply(arr_epochs[i]) # TODO at least i hope so, double check indices. 
+        
     except Exception as e:
         print("No ICs to exclude: \n", e)
+    
+    #n_exclude = [0,1,2,3,4,5]
+    n.apply(arr_epochs[i]) # TODO at least i hope so, double check indices. 
 
 clean_epochs = np.reshape(arr_epochs, (len(lstPIds),NUM_BLOCKS))
 #clean_epochs = np.reshape(arr_epochs, (2,2)) # for testing only
