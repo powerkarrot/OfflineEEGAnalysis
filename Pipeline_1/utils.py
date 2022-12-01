cee80e23f59d272c3fb4a68cddc2d5416133bb50
powerkarrot <powerkarrot@gmail.com>
2022-12-01 13:33:31 +0100

    pipeline 1 clean and bug fix

--- Pipeline_1/utils.py
+++ Pipeline_1/utils.py
@@ -1,7 +1,10 @@
 
 from itertools import chain, repeat
 import numpy as np
-    
+from Settings import *
+import mne
+from itertools import compress
+
 def get_user_input(valid_response, prompt, err_prompt):
     prompts = chain([prompt], repeat(err_prompt))
     replies = map(input, prompts)
@@ -15,6 +18,10 @@ def get_psd(instance, method, picks):
     mean_spectrum = spectrum.average()  
     return mean_spectrum.get_data(return_freqs=True)
 
+def mask_channels(target_ch):
+    mask = np.array(np.isin(channels, target_ch, invert=True), dtype = bool)
+    excl = list(compress(channels, mask))
+    return excl
 
 def psdsDB(psd):
      return 10 * np.log10(psd)
